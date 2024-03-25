9a4b7fbed713c3abc48246c4b1a2f3b099c218ae
yuuki-job <giyu0194@gmail.com>
2024-03-25 13:34:47 +0900

    Tokyoと検索すると背景画像が変化する

---
+++ 7_Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchField: UITextField!
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    //MARK: Properties
+    var weatherManager = WeatherDataManager()
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        weatherManager.delegate = self
+        searchField.delegate = self
+    }
+
+
+}
+ 
+//MARK:- TextField extension
+extension WeatherViewController: UITextFieldDelegate {
+    
+        @IBAction func searchBtnClicked(_ sender: UIButton) {
+            searchField.endEditing(true)    //dismiss keyboard
+            print(searchField.text!)
+            
+            searchWeather()
+        }
+    
+        func searchWeather(){
+            if let cityName = searchField.text{
+                weatherManager.fetchWeather(cityName)
+            }
+        }
+        
+        // when keyboard return clicked
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            searchField.endEditing(true)    //dismiss keyboard
+            print(searchField.text!)
+            
+            searchWeather()
+            return true
+        }
+        
+        // when textfield deselected
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            // by using "textField" (not "searchField") this applied to any textField in this Controller(cuz of delegate = self)
+            if textField.text != "" {
+                return true
+            }else{
+                textField.placeholder = "Type something here"
+                return false            // check if city name is valid
+            }
+        }
+        
+        // when textfield stop editing (keyboard dismissed)
+        func textFieldDidEndEditing(_ textField: UITextField) {
+    //        searchField.text = ""   // clear textField
+        }
+}
+
+//MARK:- View update extension
+extension WeatherViewController: WeatherManagerDelegate {
+    
+    func updateWeather(weatherModel: WeatherModel){
+        DispatchQueue.main.sync {
+            temperatureLabel.text = weatherModel.temperatureString
+            cityLabel.text = weatherModel.cityName
+            self.conditionImageView.image = UIImage(systemName: weatherModel.conditionName)
+            if searchField.text == "Tokyo" {
+                            self.backgroundImageView.image = UIImage(named: "backgroundTokyo")
+                        } else {
+                            self.backgroundImageView.image = UIImage(named: "background")
+                        }
+        }
+    }
+    
+    func failedWithError(error: Error){
+        print(error)
+    }
+}
+
+// MARK:- CLLocation
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    @IBAction func locationButtonClicked(_ sender: UIButton) {
+        // Get permission
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(lat, lon)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
